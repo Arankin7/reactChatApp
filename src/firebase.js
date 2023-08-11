31055e4328f8a1eb53b26346f26f801ba413f454
Arankin7 <30415670+Arankin7@users.noreply.github.com>
2023-08-11 12:12:18 -0400

    sends and recieves messages to/from the database

--- src/firebase.js
+++ src/firebase.js
@@ -1,25 +1,25 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.REACT_APP_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_APP_ID,
-  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+  apiKey: "AIzaSyAxoBSC9swW8KFkA6Rlt7c5WiMz3U3mc_E",
+  authDomain: "react-chat-63e43.firebaseapp.com",
+  projectId: "react-chat-63e43",
+  storageBucket: "react-chat-63e43.appspot.com",
+  messagingSenderId: "7465059653",
+  appId: "1:7465059653:web:c95df402f396d2cc552a2c",
+  measurementId: "G-T45MVNGYNT"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firebase Authentication and get a reference to the service
+const analytics = getAnalytics(app);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
